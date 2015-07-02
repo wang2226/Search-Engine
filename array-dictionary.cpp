41691c61848892db26fc38915fcef1931eecce4b
CS251 Account <cs251@data.cs.purdue.edu>
2015-07-02 17:41:23 -0400

    Commit search engine

---
+++ array-dictionary.cpp
@@ -0,0 +1,51 @@
+
+// Implementation of a dictionary using an array and sequential search
+// The array will be dynamically resized if necessary
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "array-dictionary.h"
+
+// Constructor
+ArrayDictionary::ArrayDictionary()
+{
+  // Add your code here
+}
+
+// Add a record to the dictionary. Returns false if key already exists
+bool
+ArrayDictionary::addRecord( KeyType key, DataType record)
+{
+        // Add your code here
+	
+	return true;
+}
+
+// Find a key in the dictionary and return corresponding record or NULL
+DataType
+ArrayDictionary::findRecord( KeyType key)
+{
+        // add your code here
+
+	return NULL;
+}
+
+// Removes one element from the table
+bool
+ArrayDictionary::removeElement(KeyType key)
+{
+        // Add your code here
+
+	return true;
+	
+}
+
+// Returns all the elements in the table as an array of strings.
+// *n is the size of the table and it is returned by reference
+KeyType *
+ArrayDictionary::keys(int * n)
+{
+        // Add yoru code here
+	return NULL;
+}
