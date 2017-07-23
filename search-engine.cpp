c2d12f4fda7dcfbeb4e2574c66f44fcc9e262d10
Haoran Wang <wang2226@data.cs.purdue.edu>
2017-07-23 16:16:32 -0400

    Commit search engine

--- search-engine.cpp
+++ search-engine.cpp
@@ -161,7 +161,6 @@ SearchEngine::dispatch( FILE * fout, const char * documentRequested)
   if (strcmp(documentRequested, "/")==0) {
     // Send initial form
     fprintf(fout, "<TITLE>CS251 Search</TITLE>\r\n");
-	//background
 	fprintf(fout, "<BODY background=\"https://news.uns.purdue.edu/images/cs.render.jpeg\"style=\"background-attachment: fixed; background-repeat:no-repeat; background-cover:cover\">\n");
     fprintf(fout, "<CENTER><H1><em>Boiler Search</em></H1>\n");
     fprintf(fout, "<H2>\n");
@@ -225,7 +224,7 @@ SearchEngine::dispatch( FILE * fout, const char * documentRequested)
 
 
   fprintf( fout, "<TITLE>Search Results</TITLE>\r\n");
-  fprintf(fout, "<BODY background=\"https://pbs.twimg.com/profile_images/772114773760704512/x0m-WBgq.jpg\">\n");
+  fprintf(fout, "<BODY link background=\"https://wallpaperscraft.com/image/glare_light_light_background_36832_2560x1600.jpg\">\n");
   fprintf( fout, "<H1> <Center><em>Boiler Search</em></H1>\n");
   fprintf( fout, "<H2> Search Results for \"%s\"</center></H2>\n", words);
 
